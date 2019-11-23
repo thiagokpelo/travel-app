3aa8ace862c8d10078bce64fa2d42c233a095cf8
Thiago Capelo <thiagokpelo@gmail.com>
2019-11-23 11:39:49 -0300

    add: periodToString method

--- app/src/main/java/dev/thiagokpelo/viagens/utils/DateUtil.java
+++ app/src/main/java/dev/thiagokpelo/viagens/utils/DateUtil.java
@@ -1,5 +1,8 @@
 package dev.thiagokpelo.viagens.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class DateUtil {
 
     public static final String PLURAL_DAYS = " dias";
@@ -10,5 +13,19 @@ public class DateUtil {
         return (daysQnt > 1)
                 ? daysQnt + PLURAL_DAYS
                 : daysQnt + SINGULAR_DAYS;
+    }
+
+    public static String periodToString(int days) {
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.DATE, days);
+
+        SimpleDateFormat dateBrazilFormat = new SimpleDateFormat("dd/MM");
+        String startDateFormatted = dateBrazilFormat.format(startDate.getTime());
+        String endDateFormatted = dateBrazilFormat.format(endDate.getTime());
+
+        return startDateFormatted
+                + " - " + endDateFormatted
+                + " de " + endDate.get(Calendar.YEAR);
     }
 }
